5797732b9b5e525a67d4530f4dda7a6e7d5f5e72
Abhijeet Singh <109523390+AbhijeetS29@users.noreply.github.com>
2023-06-09 15:39:33 +0530

    commit

--- app/src/main/java/com/abhijeet/navigation/MainActivity.kt
+++ app/src/main/java/com/abhijeet/navigation/MainActivity.kt
@@ -5,12 +5,18 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
+
 class MainActivity : AppCompatActivity() {
     lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navController=findNavController(R.id.navController)
+
+
+
             }
 
 
